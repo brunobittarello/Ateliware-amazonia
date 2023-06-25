bba511411d8cc660330ba618c3df02f43547d406
Bruno Bittarello <brunobitta@hotmail.com>
2023-06-24 23:55:10 -0300

    Removing unnecessary page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,11 +27,6 @@ public class HomeController : Controller
         return await _coordinateService.CalculateRoute(dataDto.Start, dataDto.PickUp, dataDto.Destination);
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
